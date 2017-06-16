75f29f3b09b4b0696dce4c464d935822e0f6a9d3
Helena Black <helenajblack@yahoo.com>
2017-06-16 13:55:41 -0500

    fixed bugs and dom display

--- public/scripts/peepsApp.js
+++ public/scripts/peepsApp.js
@@ -1,18 +1,20 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', function( $http, PeepsService ){
-var vm = this;
-vm.newPeep = function(){
-console.log( 'in newPeep' );
-var peepToAdd = {
-name= vm.nameIn,
-location= vm.locationIn
-}; //end peepToAdd
-PeepsService.addPeep( peepToAdd );
-}; //end whereMyPeepsAt
-vm.whereMyPeepsAt = function(){
-console.log( 'in whereMyPeepsAt' );
-PeepsService.getPeeps().then( function(){
-console.log( 'back in controller:', PeepsService.allMyPeeps );
-});
-}; //end whereMyPeepsAt
+var myApp = angular.module('myApp', []);
+myApp.controller('WhereMyPeeps', function($http, PeepsService) {
+  var vm = this;
+  vm.peeps = [];
+  vm.newPeep = function() {
+    console.log('in newPeep');
+    var peepToAdd = {
+      name: vm.nameIn,
+      location: vm.locationIn
+    }; //end peepToAdd
+    PeepsService.addPeep(peepToAdd);
+  }; //end whereMyPeepsAt
+  vm.whereMyPeepsAt = function() {
+    console.log('in whereMyPeepsAt');
+    PeepsService.getPeeps().then(function() {
+      console.log('back in controller:', PeepsService.allMyPeeps);
+      vm.peeps = PeepsService.allMyPeeps.data;
+    });
+  }; //end whereMyPeepsAt
 }); // end controller
